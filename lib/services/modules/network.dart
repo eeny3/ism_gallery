27e6c908bb17d6cb22a03c874a4efe7dc74846b7
Bek <xabuovx@gmail.com>
2024-04-19 18:07:58 +0600

    changing logging to package

--- lib/services/modules/network.dart
+++ lib/services/modules/network.dart
@@ -1,18 +1,18 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:ism/services/networking/interceptors/auth_interceptor.dart';
-import 'package:ism/services/networking/interceptors/logging_interceptor.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 @module
 abstract class NetworkModule {
   @lazySingleton
   Dio getDio() => Dio()
     ..options.baseUrl = 'https://pixabay.com/api/'
-    ..options.headers['accept'] = 'application/json'
+    ..options.headers['accept'] = '*/*'
     ..interceptors.addAll(
       [
         AuthInterceptor(),
-        LoggingInterceptor(),
+        PrettyDioLogger(),
       ],
     );
 }
